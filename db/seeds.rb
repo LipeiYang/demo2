b83457a23ea6debd4d4a1ec23d53f167877127cb
Lipei Yang <lipei.yang@gmail.com>
2010-07-10 18:38:55 +0800

    1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+[ { :name => 'A3', :price => 4.8 },
+  { :name => '中碳', :price => 5 },
+  { :name => '氯12', :price => 14 },
+  { :name => '42ln', :price => 8 } ].each do |p|    
+  Product.find_or_create_by_name p    
+end
+
