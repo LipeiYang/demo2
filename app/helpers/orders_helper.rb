7c9eeaa537b19c317053e5fdba3b8bd39acca9cc
Lipei Yang <lipei.yang@gmail.com>
2010-07-12 01:45:57 +0800

    1

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,12 @@
 module OrdersHelper
+  def customer_unpaied_list
+    rlt = {}
+    @orders.each do |o|
+      if o.is_paied == 'no'      
+        rlt[o.customer.name]||=0
+        rlt[o.customer.name]+=o.totale
+      end
+    end
+    rlt
+  end
 end
