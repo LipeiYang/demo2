4bf6b163e9e68752e0345cbf3cc1b1c6d9662f6e
Lipei Yang <lipei.yang@gmail.com>
2010-07-11 23:52:46 +0800

    1

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,11 @@ class Order < ActiveRecord::Base
   belongs_to :customer
   belongs_to :product
   
+  
+  def material_fee
+    price*volume
+  end
+  
   def totale
     price*volume+manfee
   end
