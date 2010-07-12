9d297ccbcb6446acfe995e64dd0330fe74de4fea
Lipei Yang <lipei.yang@gmail.com>
2010-07-12 21:45:03 +0800

    1

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,4 +1,5 @@
 module OrdersHelper
+
   def customer_unpaied_list
     rlt = {}
     @orders.each do |o|
@@ -7,6 +8,17 @@ module OrdersHelper
         rlt[o.customer.name]+=o.totale
       end
     end
-    rlt
+    rlt.sort{|a,b| a[1]<=>b[1]}
+  end
+  
+  def product_position_list
+    rlt = {}
+    @orders.each do |o|
+      if o.material_fee > 0      
+        rlt[o.product.name]||=0
+        rlt[o.product.name]+=o.material_fee
+      end
+    end
+    rlt.sort{|a,b| a[1]<=>b[1]}
   end
 end
