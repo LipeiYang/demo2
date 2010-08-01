16e00dc8f11909aa5d44e7edae743e541beb376a
Lipei <lipei.yang@gmail.com>
2010-08-01 22:17:21 +0800

    1

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -52,19 +52,30 @@ module OrdersHelper
     end
     rlt.sort_by{|a| -a[1]}
   end
-  
+    
   def product_position_list
     rlt = {}
     @orders.each do |o|
       if o.material_fee > 0      
-        rlt[o.product.name]||={:total_fee=>0, :total_volume=>0, :total_cost=>0, :total_profit=>0}
-        rlt[o.product.name][:total_fee]+=o.material_fee
+        rlt[o.product.name]||={:total_mat_fee=>0, :total_volume=>0, :total_cost=>0, :total_profit=>0}
+        rlt[o.product.name][:total_mat_fee]+=o.material_fee
         rlt[o.product.name][:total_volume]+=o.volume
         rlt[o.product.name][:total_cost]+=o.material_cost
         rlt[o.product.name][:total_profit]+=o.material_fee-o.material_cost
       end
     end
-    
+    rlt.sort_by{|a| -a[1][:total_profit]}
+  end
+  
+  def customer_position_list
+    rlt = {}
+    @orders.each do |o|
+      rlt[o.customer.name]||= {:total_mat_fee=>0, :total_mat_profit=>0, :total_man_fee=>0, :total_profit=>0}
+      rlt[o.customer.name][:total_mat_fee]+=o.material_fee
+      rlt[o.customer.name][:total_mat_profit]+=o.material_fee-o.material_cost
+      rlt[o.customer.name][:total_man_fee]+=o.manfee
+      rlt[o.customer.name][:total_profit]+=o.totale-o.material_cost
+    end
     rlt.sort_by{|a| -a[1][:total_profit]}
   end
 end
