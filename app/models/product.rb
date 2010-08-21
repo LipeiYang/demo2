9cba8b2dd6b7a701c9c2336e9f7f4eb8f3fc4f73
Lipei Yang <lipei.yang@gmail.com>
2010-08-21 20:48:17 +0800

    1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
   has_many :orders
   has_many :purchases
+  
+  def weighted_cost
+    if purchases.blank?
+      ""
+    else
+      purchases.to_a.sum { |t| t.volume*t.price }/purchases.to_a.sum { |t| t.volume }
+    end
+  end
 end
