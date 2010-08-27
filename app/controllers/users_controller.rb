b0d0bea4eab8ff3ce06362a5c624f8ed85371440
Lipei Yang <lipei.yang@gmail.com>
2010-08-28 00:33:25 +0800

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
 
-    # respond_to do |format|
-    #   format.html # new.html.erb
-    #   format.xml  { render :xml => @user }
-    # end
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @user }
+    end
   end
 
   # GET /users/1/edit
