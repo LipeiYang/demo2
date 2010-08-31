026bfd5232b403d785aa7bc3001632cab5592445
Lipei Yang <lipei.yang@gmail.com>
2010-08-31 20:49:40 +0800

    1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 require 'rails_extensions'
 module ApplicationHelper
+  
   def f(num)
     h number_with_precision(num, :precision => 2, :separator => ".")
   end
+  
+  def title(page_title)  
+    content_for(:title) { page_title }  
+  end
+    
 end
