d647e18ecc0a2abf5e5f19ca70cfa124d121893b
Lipei Yang <lipei.yang@gmail.com>
2010-10-08 00:36:16 +0800

    add total will rec and to pay

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,5 @@
 module CustomersHelper
+  def total_amount_will_receive
+    @customers.sum(&:amount_will_receive)
+  end
 end
--- app/helpers/suppliers_helper.rb
+++ app/helpers/suppliers_helper.rb
@@ -1,2 +1,5 @@
 module SuppliersHelper
+  def total_amount_to_pay
+    @suppliers.sum(&:amount_to_pay)
+  end
 end
