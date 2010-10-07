e6f9f009ef4f2c75e38de80ca7d8dda11369b9a5
Lipei Yang <lipei.yang@gmail.com>
2010-10-07 21:39:59 +0800

    mod supplier migration

---
+++ db/migrate/20101007133812_redo_supplier_cutoff.rb
@@ -0,0 +1,18 @@
+class RedoSupplierCutoff < ActiveRecord::Migration
+  def self.up
+    remove_column :suppliers, :cutoff_date
+    remove_column :suppliers, :unpaid_amount
+    add_column :suppliers, :cutoff_date, :date
+    add_column :suppliers, :unpaied_amount, :float, :defaut=>0
+    Supplier.all.each do |o|
+      o.cutoff_date = Date.civil(2010,1,1)
+      o.unpaied_amount = 0
+      o.save
+    end
+  end
+
+  def self.down
+    remove_column :suppliers, :cutoff_date
+    remove_column :suppliers, :unpaied_amount
+  end
+end
