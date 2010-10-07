288f43b76221544fc1e8fdb1ca5f093d5bf6c996
Lipei Yang <lipei.yang@gmail.com>
2010-10-07 21:24:07 +0800

    mod supplier migration

--- db/migrate/20101002171058_add_cutoff_to_supplier.rb
+++ db/migrate/20101002171058_add_cutoff_to_supplier.rb
@@ -1,11 +1,16 @@
 class AddCutoffToSupplier < ActiveRecord::Migration
   def self.up
     add_column :suppliers, :cutoff_date, :date
-    add_column :suppliers, :unpaid_amount, :integer, :defaut=>0
+    add_column :suppliers, :unpaied_amount, :float, :defaut=>0
+    Supplier.all.each do |o|
+      o.cutoff_date = Date.civil(2010,1,1)
+      o.unpaied_amount = 0
+      o.save
+    end
   end
 
   def self.down
     remove_column :suppliers, :cutoff_date
-    remove_column :suppliers, :unpaid_amount
+    remove_column :suppliers, :unpaied_amount
   end
 end
--- db/migrate/20101003064217_rename_supplier_unpaied.rb
+++
@@ -1,9 +0,0 @@
-class RenameSupplierUnpaied < ActiveRecord::Migration
-  def self.up
-    rename_column :suppliers, :unpaid_amount, :unpaied_amount
-  end
-
-  def self.down
-    rename_column :suppliers, :unpaied_amount, :unpaid_amount
-  end
-end
