2a829cb793df7fb4860cabc05fc8bcbbfd08b10e
Lipei Yang <lipei.yang@gmail.com>
2010-10-21 00:27:05 +0800

    ext

--- lib/rails_extensions.rb
+++ lib/rails_extensions.rb
@@ -13,3 +13,15 @@ module ActionView
     end
   end
 end
+
+module ActiveRecord
+  class TableLessBase < ActiveRecord::Base
+    
+    def self.columns() @columns ||= []; end  
+  
+    def self.column(name, sql_type = nil, default = nil, null = true)  
+      columns << ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)  
+    end  
+    
+  end 
+end
