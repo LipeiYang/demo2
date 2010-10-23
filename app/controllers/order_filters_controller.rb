58875c2a551d8a2ef514a7eb58e45d302a25066b
Lipei Yang <lipei.yang@gmail.com>
2010-10-23 13:30:21 +0800

    filter

--- app/controllers/order_filters_controller.rb
+++ app/controllers/order_filters_controller.rb
@@ -2,9 +2,7 @@ class OrderFiltersController < ApplicationController
 
   def create
     cookies[:by_esti_cost] = params[:by_esti_cost]
-    @order_filter = OrderFilter.new(params[:order_filter])
-    puts "@order_filter-->#{@order_filter.s_paid==''}"
-    session[:criteria_order] = @order_filter
+    session[:criteria_order] = OrderFilter.new(params[:order_filter])
     redirect_to orders_path
   end
 
