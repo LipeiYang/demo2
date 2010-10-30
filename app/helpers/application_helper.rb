252fb4b300f9d14ea21df672d3406f64ee9e3815
Lipei Yang <lipei.yang@gmail.com>
2010-10-30 14:03:54 +0800

    print css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,27 @@ module ApplicationHelper
   end
   
   def title(page_title)  
-    content_for(:title) { page_title }  
+    content_for(:title) { t(page_title) }  
   end
-    
+
+  def render_new_link(name, url)
+    render :partial => "partials/new_link_facade", :locals =>{ :content => link_to(t(name), url) }
+  end
+  
+  def render_edit_link(url)
+    render :partial => "partials/edit_link_facade", :locals =>{ :content => link_to(t('Edit'), url) }
+  end
+
+  def render_delete_link(url)
+    render :partial => "partials/delete_link_facade", :locals =>{ :content => 
+      link_to(t('Destroy'), url, :confirm => t('sure?'), :method => :delete) }
+  end
+
+  def render_edit_link(url, name='Edit')
+    render :partial => "partials/edit_link_facade", :locals =>{ :content => link_to(t(name), url) }
+  end
+  
+  def render_filter(filter_form)
+    render :partial => "partials/filter_facade", :locals =>{ :content => render(:partial => filter_form) }
+  end
 end
